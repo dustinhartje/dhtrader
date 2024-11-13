3e5d34e73df7f6bc55054f0e115a860c595d18a7
Dustin Hartje <dustinhartje@gmail.com>
2024-11-13 00:19:41 -0500

    more time management functions

--- dhutil.py
+++ dhutil.py
@@ -1,7 +1,7 @@
 from datetime import datetime as dt
 
 TIMEFRAMES = ['1m', '5m', '15m', '1h', '1d', '1w', '1mo']
-TRADING_HOURS = ['regular', 'extended']
+TRADING_HOURS = ['rth', 'eth']
 
 def valid_timeframe(t):
     if t in TIMEFRAMES:
@@ -9,7 +9,9 @@ def valid_timeframe(t):
     else:
         return False
 
+
 def dt_as_dt(d):
+    """return a datetime object regardless of datetime or string input"""
     if isinstance(d, dt):
         return d
     else:
@@ -17,7 +19,38 @@ def dt_as_dt(d):
 
 
 def dt_as_str(d):
+    """return a string object regardless of datetime or string input"""
     if isinstance(d, str):
         return d
     else:
         return d.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def dt_to_epoch(d):
+    """return an epoch integer from a datetime or string"""
+    return int(dt_as_dt(d).strftime('%s'))
+
+
+def dt_from_epoch(d):
+    """return a datetime object from an epoch integer"""
+    return dt.fromtimestamp(d)
+
+
+def test_basics():
+    ts = "2024-01-01 12:30:00"
+    print(f"Starting with string: {ts} {type(ts)}")
+    td = dt_as_dt(ts)
+    print(f"Converted to datetime: {td} {type(td)}")
+    tds = dt_as_str(td)
+    print(f"Converted back to string: {tds} {type(tds)}")
+    tse = dt_to_epoch(ts)
+    print(f"String converted to epoch: {tse} {type(tse)}")
+    tde = dt_to_epoch(td)
+    print(f"Datetime converted to epoch: {tde} {type(tde)}")
+    t=dt_from_epoch(tde)
+    print(f"Epoch converted back to datetime: {t} {type(t)}")
+
+if __name__ == '__main__':
+    test_basics()
+
+
