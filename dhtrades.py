48198ee17a8586f9fe6287180012e95ebe47f70b
Dustin Hartje <dustinhartje@gmail.com>
2025-01-25 09:01:55 -0500

    cleanup/catchup Trade() with evolving standards in prep for OOP backtesting development

--- dhtrades.py
+++ dhtrades.py
@@ -1,8 +1,41 @@
 import json
 from datetime import datetime as dt
+import dhutil as dhu
 
 
 class Trade():
+    """Represents a single trade that could have been made.
+
+    Attributes:
+        open_dt (str): datetime trade was initiated
+        direction (str): 'long' or 'short'
+        entry_price (float): price at which trade was initiated
+        stop_target (float): price at which trade would auto exit at loss
+        prof_target (float): price at which trade would auto exit at profit
+        open_drawdown (float): account drawdown level at time of trade open
+        close_drawdown (float): account drawdown level after trade was closed
+        close_dt (str): datetime trade was closed
+        created_dt (str): datetime this object was created
+        exit_price (float): price at which trade was closed
+        gain_loss (float): account balance gain or loss resulting from trade
+        stop_ticks (float): number of ticks to reach stop_target from entry,
+            primarily used as trade rules identification during analysis
+        prof_ticks (float): number of ticks to reach prof_target from entry,
+            primarily used as trade rules identification during analysis
+        offset_ticks (float): number of ticks away from target price that
+            this trade made entry, primarily used as trade rules
+            identification during analysis
+        drawdown_impact (float): potential impact on trailing drawdown from
+            entry_price to max unrealized profit seen.  Added to gain_loss in
+            a losing trade to determine close_drawdown
+        symbol (str): ticker being traded
+        contracts (int): number of contracts traded
+        contract_value (float): value per contract
+        is_open (bool): True if trade has not yet been closed via .close()
+        successful (bool): True if trade was profitable (rename to profitable?)
+        trade_name (str): Human friendly (?) trade name
+        trade_version (str): Trade version if applicable
+    """
     def __init__(self,
                  open_dt: str,
                  direction: str,
@@ -10,19 +43,18 @@ class Trade():
                  stop_target: float,
                  prof_target: float,
                  open_drawdown: float,
-                 close_drawdown: float,
-                 max_drawdown: float,
+                 close_drawdown: float = None,
                  close_dt: str = None,
-                 evaluation_dt: str = None,
+                 created_dt: str = None,
                  exit_price: float = None,
                  gain_loss: float = None,
-                 stop_ticks: float = 0,
-                 prof_ticks: float = 0,
-                 offset_ticks: float = 0,
-                 drawdown_impact: float = 0,
-                 ticker: str = 'ES',
+                 stop_ticks: float = float(0),
+                 prof_ticks: float = float(0),
+                 offset_ticks: float = float(0),
+                 drawdown_impact: float = float(0),
+                 symbol: str = 'ES',
                  contracts: int = 1,
-                 contract_value: float = 50,
+                 contract_value: float = float(50),
                  is_open: bool = True,
                  successful: bool = None,
                  trade_name: str = None,
@@ -31,21 +63,20 @@ class Trade():
 
         self.open_dt = open_dt
         self.close_dt = close_dt
-        self.evaluation_dt = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.created_dt = dhu.dt_as_str(dt.now())
         self.direction = direction
-        self.entry_price: float = entry_price
+        self.entry_price = entry_price
         self.stop_target = stop_target
         self.prof_target = prof_target
         self.exit_price = exit_price
         self.open_drawdown = open_drawdown
         self.close_drawdown = close_drawdown
-        self.max_drawdown = max_drawdown
         self.gain_loss = gain_loss
         self.stop_ticks = stop_ticks
         self.prof_ticks = prof_ticks
         self.offset_ticks = offset_ticks
         self.drawdown_impact = drawdown_impact
-        self.ticker = ticker
+        self.symbol = symbol
         self.contracts = contracts
         self.contract_value = contract_value
         self.is_open = is_open
@@ -53,126 +84,35 @@ class Trade():
         self.trade_name = trade_name
         self.trade_version = trade_version
 
-    def csv_list(self):
-        return [self.open_dt,
-                self.close_dt,
-                self.direction,
-                self.entry_price,
-                self.stop_target,
-                self.prof_target,
-                self.exit_price,
-                self.open_drawdown,
-                self.close_drawdown,
-                self.max_drawdown,
-                self.gain_loss,
-                self.stop_ticks,
-                self.prof_ticks,
-                self.offset_ticks,
-                self.drawdown_impact,
-                self.ticker,
-                self.contracts,
-                self.contract_value,
-                self.is_open,
-                self.successful,
-                self.evaluation_dt,
-                self.trade_name,
-                self.trade_version,
-                ]
+    def __str__(self):
+        return str(self.to_clean_dict())
 
-    def csv_header(self):
-        return ['open_dt',
-                'close_dt',
-                'direction',
-                'entry_price',
-                'stop_target',
-                'prof_target',
-                'exit_price',
-                'open_drawdown',
-                'close_drawdown',
-                'max_drawdown',
-                'gain_loss',
-                'stop_ticks',
-                'prof_ticks',
-                'offset_ticks',
-                'drawdown_impact',
-                'ticker',
-                'contracts',
-                'contract_value',
-                'is_open',
-                'successful',
-                'evaluation_dt',
-                'trade_name',
-                'trade_version',
-                ]
+    def __repr__(self):
+        return str(self)
 
-    def from_json(self, json_str: str = None, file_path: str = None):
-        """Loads data into this Trade object from a json string or file"""
-        if json_str is None and file_path is None:
-            raise Exception('Must provide either json_str or file_path')
-        if json_str is not None and file_path is not None:
-            raise Exception('Provide json_str OR file_path, not both')
-
-        if json_str is not None:
-            d = json.loads(json_str)
-        else:
-            with open(file_path) as f:
-                d = json.load(f)
-
-        self.open_dt = d['open_dt']
-        self.close_dt = d['close_dt']
-        self.evaluation_dt = d['evaluation_dt']
-        self.direction = d['direction']
-        self.entry_price = d['entry_price']
-        self.stop_target = d['stop_target']
-        self.prof_target = d['prof_target']
-        self.exit_price = d['exit_price']
-        self.open_drawdown = d['open_drawdown']
-        self.close_drawdown = d['close_drawdown']
-        self.max_drawdown = d['max_drawdown']
-        self.gain_loss = d['gain_loss']
-        self.stop_ticks = d['stop_ticks']
-        self.prof_ticks = d['prof_ticks']
-        self.offset_ticks = d['offset_ticks']
-        self.drawdown_impact = d['drawdown_impact']
-        self.ticker = d['ticker']
-        self.contracts = d['contracts']
-        self.contract_value = d['contract_value']
-        self.is_open = d['is_open']
-        self.successful = d['successful']
-        return True
-
-    def to_json(self, file_path: str = None):
+    def to_json(self):
         """returns a json version of this Trade object while normalizing
-        custom types (like datetime to string) and optionally writing the
-        result to a file as well"""
-        if not isinstance(self.open_dt, str):
-            self.open_dt = dt.strftime(
-                    self.open_dt,
-                    "%Y-%m-%d %H:%M:%S")
-        if not isinstance(self.close_dt, str):
-            self.close_dt = dt.strftime(
-                    self.close_dt,
-                    "%Y-%m-%d %H:%M:%S")
-        if not isinstance(self.evaluation_dt, str):
-            self.evaluation_dt = dt.strftime(
-                    self.evaluation_dt,
-                    "%Y-%m-%d %H:%M:%S")
-        if file_path is not None:
-            with open(file_path, 'w', newline='') as f:
-                json.dump(self.__dict__,
-                          f,
-                          indent=2,
-                          )
+        custom types for json compatibility (i.e. datetime to string)"""
+        # Make sure dates are strings not datetimes
+        if self.open_dt is not None:
+            self.open_dt = dhu.dt_as_str(self.open_dt)
+        if self.close_dt is not None:
+            self.close_dt = dhu.dt_as_str(self.close_dt)
+        if self.created_dt is not None:
+            self.created_dt = dhu.dt_as_str(self.created_dt)
+
         return json.dumps(self.__dict__)
 
     def to_clean_dict(self):
         """Converts to JSON string then back to a python dict.  This helps
         to normalize types (I'm looking at YOU datetime) while ensuring
         a portable python data structure"""
+
         return json.loads(self.to_json())
 
     def update_drawdown(self,
-                        price_seen: float):
+                        price_seen: float,
+                        ):
         """Takes a price, typically the candle high or low, and updates
            the drawdown impact accordingly"""
         # NOTE This only updates the drawdown_impact and is meant for
@@ -185,12 +125,8 @@ class Trade():
         # In case of a short trade this is reversed
         if self.direction == 'short':
             price_diff = price_diff * -1
-        # print(f"drawdown_impact={self.drawdown_impact}")
         # Use the worst of the current and prior worse drawdown impact
         self.drawdown_impact = min(self.drawdown_impact, price_diff)
-        # print(f"price_seen={price_seen} | "
-        #       f"price_diff={price_diff} | entry_price={self.entry_price} "
-        #       f"drawdown_impact={self.drawdown_impact}")
 
     def close(self,
               price: float,
@@ -221,7 +157,36 @@ class Trade():
             self.drawdown_impact = ((self.drawdown_impact
                                      * contract_multiplier)
                                     + self.gain_loss)
-        self.close_drawdown = min(self.open_drawdown + self.drawdown_impact,
-                                  self.max_drawdown)
-        # print(f"CLOSED: entry={self.entry_price} exit={self.exit_price} "
-        #       f"drawdown_impact={self.drawdown_impact}")
+        self.close_drawdown = self.open_drawdown + self.drawdown_impact
+        print(f"open_drawdown {self.open_drawdown} drawdown_impact "
+              f"{self.drawdown_impact} close_drawdown {self.close_drawdown}")
+
+
+def test_basics():
+    """Basics tests used during development and to confirm simple functions
+    working as expected"""
+    # TODO LOWPRI make these into unit tests some day
+
+    # Trades
+    t = Trade(open_dt="2025-01-02 12:00:00",
+              direction="long",
+              entry_price=5001.50,
+              stop_target=4995,
+              prof_target=5010,
+              open_drawdown=1000,
+              )
+    print(f"Created unclosed test trade:\n{t}")
+    print("Updating drawdown_impact")
+    t.update_drawdown(price_seen=5009)
+    print(t)
+    print("Closing at a loss")
+    t.close(price=4995, time="2025-01-02 12:45:00")
+    print(t)
+
+    # TradeSeries
+
+    # Backtesters
+
+
+if __name__ == '__main__':
+    test_basics()
