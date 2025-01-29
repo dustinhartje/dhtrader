45b6622ce24383235b11799b11f39c04340dee30
Dustin Hartje <dustinhartje@gmail.com>
2025-01-29 04:02:27 -0500

    debugging output details for integrity issues

--- dhstore.py
+++ dhstore.py
@@ -448,7 +448,8 @@ def review_candles(timeframe: str,
                     status = "ERROR"
                     if err_msg != "":
                         err_msg += " || "
-                    err_msg += f"{k} summary data does not match expected"
+                    err_msg += f"{k} summary data does not match expected: "
+                    err_msg += f"{summary_data[k]} != {v}"
         else:
             status = "UNKNOWN"
             err_msg = f"Expected data not defined for timeframe: {timeframe}"
