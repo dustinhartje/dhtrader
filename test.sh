bdb4e306be3dd8ab72158845d624c7a72908ecf6
Dustin Hartje <dustinhartje@gmail.com>
2025-04-03 22:10:43 -0400

    help and wildcard args for test runner script, mongo collection tweaks

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-pytest -sv $1 --durations=0
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "-?" ]; then
+    echo "To exit on the first error: -x"
+    echo "To run a single test file: test.sh tests/test_file.py"
+    echo "To run a single test: test.sh tests/test_file.py::test_name"
+else
+    pytest -sv --durations=0 "$@"
+fi
 
 ls tests/hide_* &>/dev/null
-test $? -ne 0 || echo "Hidden test files detected (./tests/hide_*.py.  Is this intentional?"
-
-#if [ $? -ne 0 ]; then
-#    echo "Hidden test files detected (./tests/hide_*.py.  Is this intentional?"
-#fi
+test $? -ne 0 || echo "Hidden test files detected (./tests/hide_*.py).  Is this intentional?"
