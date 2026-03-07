bd810de8613a0c102e1dede8b6d196156518e627
Dustin Hartje <dustinhartje@gmail.com>
2026-03-06 22:30:33 -0500

    remove slow markers for tests now performing much faster

--- tests/test_dhtypes_Indicator.py
+++ tests/test_dhtypes_Indicator.py
@@ -211,7 +211,6 @@ def test_Indicator_storage_spotcheck_ES_eth_15m_EMA_close_l9_s2():
 
 
 # 15m ETH 20
-@pytest.mark.slow
 def Indicator_spotcheck_ES_eth_15m_EMA_close_l20_s2(i):
     # Sun 12/8/24
     assert i.get_datapoint(dt="2024-12-08 18:00:00").value == 6096.55
@@ -281,7 +280,6 @@ def test_Indicator_storage_spotcheck_ES_eth_15m_EMA_close_l20_s2():
 
 
 # e1h ETH 9
-@pytest.mark.slow
 def Indicator_spotcheck_ES_eth_e1h_EMA_close_l9_s2(i):
     # Sun-Sat - first & last candles, rando in the middle, rando in closed
     # Sun 11/10/24
@@ -350,7 +348,6 @@ def test_Indicator_storage_spotcheck_ES_eth_e1h_EMA_close_l9_s2():
 
 
 # e1h ETH 20
-@pytest.mark.slow
 def Indicator_spotcheck_ES_eth_e1h_EMA_close_l20_s2(i):
     # Sun-Sat - first & last candles, rando in the middle, rando in closed
     # Sun 10/13/24
