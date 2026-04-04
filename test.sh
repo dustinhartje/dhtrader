898e5f1febe997253093bbd1a8c00302d4835aba
Dustin Hartje <dustinhartje@gmail.com>
2026-04-04 14:24:00 -0400

    cleaner test output

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@
 # Show help if requested anywhere in arguments
 for a in "$@"; do
     if [ "$a" = "-h" ] || [ "$a" = "--help" ] || [ "$a" = "-?" ]; then
+        echo "To show full error output: test.sh -v"
         echo "To exit on the first error: test.sh -x"
         echo "To run a single test file: test.sh tests/test_file.py"
         echo "To run a single test: test.sh tests/test_file.py::test_name"
@@ -11,7 +12,7 @@ for a in "$@"; do
         echo "To run only specific marks: test.sh -m 'storage'"
         echo "To run only without specific marks: test.sh -m 'not storage'"
         echo ""
-        echo "Marks are listed in setup.cfg [tool:pytest] section or grep for '@pytest.mark'"
+        echo "Marks are listed in setup.cfg/pytest.ini or grep for '@pytest.mark'"
         exit 0
     fi
 done
@@ -37,7 +38,7 @@ if [ "$FILTER_NOT_SLOW" = true ]; then
     EXTRA_MARKS+=("-m" "not slow")
 fi
 
-pytest -svv --durations=0 --show-capture=stdout "${EXTRA_MARKS[@]}" "${ARGS[@]}"
+pytest -sv -rA --tb=short --durations=0 --show-capture=stdout "${EXTRA_MARKS[@]}" "${ARGS[@]}"
 
 ls tests/hide_* &>/dev/null
 test $? -ne 0 || echo "Hidden test files detected (./tests/hide_*.py).  Is this intentional?"
